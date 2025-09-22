62195b7fe5b82278953bb686f79f3ea29f338327
obasi-1 <obasonz61@gmail.com>
2025-09-22 03:34:48 +0100

    Fix Comment __str__ to use user instead of author

--- users/models.py
+++ users/models.py
@@ -19,7 +19,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.id}"
+        return f"Comment by {self.user.username} on {self.post.id}"
     
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
